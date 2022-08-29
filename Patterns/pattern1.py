ade2300da13bfe299ed439a98f89e56b160cc246
CoderSpidey <112414000+CoderSpidey@users.noreply.github.com>
2022-08-29 21:54:51 +0530

    Add files via upload

---
+++ Patterns/pattern1.py
@@ -0,0 +1,8 @@
+# Method - 1: Standard Way
+# for i in range(5):
+#     for j in range(5):
+#         print('*', end = '')
+#     print()
+# Method - 2: Pythonic way
+for i in range(5):
+    print('*' * 5)
---
+++ Patterns/pattern2.py
@@ -0,0 +1,7 @@
+# for i in range(5):
+#     for j in range(i+1):
+#         print('*', end = '')
+#     print()
+# Method - 2: Pythonic Way
+for i in range(5):
+    print('*' * (i+1))
